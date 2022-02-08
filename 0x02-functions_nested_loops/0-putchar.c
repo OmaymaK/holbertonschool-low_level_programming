27677733f83751bd1484eb9d8f320855000e3d36
OmaymaK <emmablink050@gmail.com>
2022-02-08 03:26:58 -0800

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\_putchar\n");
-return (0);
+	char ch[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+	return (0);
 }
