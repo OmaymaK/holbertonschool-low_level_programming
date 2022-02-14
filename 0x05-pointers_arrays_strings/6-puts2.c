85c8bdb40eec0d9885f21a9ee41c4a69aee509a5
OmaymaK <emmablink050@gmail.com>
2022-02-14 10:10:49 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int _strlen(char *s);
+/**
+ * puts2 - function that prints a string, in reverse.
+ * @str: pointer of a string.
+ */
+void puts2(char *str)
+{
+	int c;
+	int len = _strlen(str);
+
+	for (c = 0; c < len; c+= 2)
+		_putchar(str[c]);
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
