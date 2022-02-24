26e7e46917753a2dcde82b49db8c92628f50efb1
OmaymaK <emmablink050@gmail.com>
2022-02-24 11:59:37 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,14 +2,18 @@
 int _checking_root(int n, int i);
 /**
  * _checking_root - checks the square root of n
- * _sqrt_recursion - returns the natural square root of a number
  * @n number that we will get the square root of
  * @i square root of n
- * Return: square root , -1 if error
+ * Return: i and n
  */
 int _sqrt_recursion(int n)
+/**
+ *  _sqrt_recursion - returns the natural square root of a number
+ *  @n number that we will get its square root
+ *  Return: square root otherwise -1 if error
+ */
 {
-	return (_checking_root(n, 1)); 
+	return (_checking_root(n, 1));
 }
 int _checking_root(int n, int i)
 {
