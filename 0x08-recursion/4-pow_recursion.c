9475a104b4dc2670451ede18f3fb893f385f7106
OmaymaK <emmablink050@gmail.com>
2022-02-24 05:51:01 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - that returns the value of x raised to the power of y
  * @x: variable
- * @y: power of x 
+ * @y: power of x
  * Return: value
  */
 int _pow_recursion(int x, int y)
@@ -17,7 +17,7 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		return (x * _pow_recursion (x,(y - 1)));
+		return (x * _pow_recursion(x, (y - 1)));
 	}
 }
 
