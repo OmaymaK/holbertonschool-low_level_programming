84876291e4b2d38c8c9b4f49a6df14c15394defa
OmaymaK <emmablink050@gmail.com>
2022-02-25 03:06:09 -0800

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array of size argc
  * Return: Program name followed by new line
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
