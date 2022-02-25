d43431b3c66c4aae961232c1e3935cec4801b5d1
OmaymaK <emmablink050@gmail.com>
2022-02-25 03:01:32 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - checks the code
+ * @argc: the number of command line arguments
+ * @argv: unused variable
+ * Return: 0
+ */
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
