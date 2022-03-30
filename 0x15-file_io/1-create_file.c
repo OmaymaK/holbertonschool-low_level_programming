644a875c9782af5218bc20d551d14fafc0dddd0d
OmaymaK <emmablink050@gmail.com>
2022-03-30 11:31:37 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_CREAT | O_RDWR, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content != NULL)
 	{
 		while (text_content[l] != '\0')
