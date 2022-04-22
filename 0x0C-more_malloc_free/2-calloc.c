6a8eb9fd5eb76cc7c667ddc86797a0699c299953
OmaymaK <emmablink050@gmail.com>
2022-04-22 16:00:06 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,11 +7,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
+	char *cont;
+	unsigned int i;
 	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
+	cont = s;
+	for (i = 0; i < nmemb * size; i++)
+		cont[i] = '\0';
 	return (s);
 }
