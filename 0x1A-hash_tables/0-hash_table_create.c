35756eafa38d12c2212b018bd01a2692b14bd892
OmaymaK <emmablink050@gmail.com>
2022-05-27 21:47:40 +0100

    hash table creation

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size: hash table's size.
+ * Return: hash table
+ */
+
+hash_table_t *hash_table_ciureate(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *new_hash = NULL;
+
+	if (size == 0)
+		return (NULL);
+	new_hash = calloc(1, sizeof(hash_table_t));
+	if (new_hash == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	new_hash->array = calloc(size, sizeof(sigset_t *));
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_hash->array[i] = NULL;
+	new_hash->size = size;
+	return (new_hash);
+}
