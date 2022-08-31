6f778e12f4a0505e4d2f8bf69361130f177e2102
OmaymaK <emmablink050@gmail.com>
2022-08-31 13:52:29 +0100

    betty fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,29 +14,29 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0, r = (size - 1);
 	size_t m, i;
-	
+
 	if (array == NULL)
 		return (-1);
 
 	while (l <= r)
 	{
 		m = ((l + r) / 2);
-		
+
 		printf("Searching in array: ");
-		
+
 		for (i = l; i < r ; i++)
 		{
 			printf("%i, ", array[i]);
 		}
-		
+
 		printf("%i\n", array[r]);
 
 		if (value > array[m])
 			l = (m + 1);
-		
+
 		else if (value < array[m])
 			r = (m - 1);
-		
+
 		else
 			return (m);
 	}
